033f28b45d1ef09267bfc2d8205108ffd133a5a8
brianliu <hong86323@gmail.com>
2024-11-17 21:15:40 +0800

    add github copilot

---
+++ lua/custom/plugins/github_copilot.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    'zbirenbaum/copilot.lua',
+    opts = {},
+  },
+  {
+    'zbirenbaum/copilot-cmp',
+    opts = {},
+    dependencies = {
+      'zbirenbaum/copilot.lua',
+    },
+  },
+}
