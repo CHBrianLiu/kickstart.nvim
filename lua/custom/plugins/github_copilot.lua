89b5abab043c845be30eea483aa041cfc7272af2
brianliu <hong86323@gmail.com>
2024-12-02 00:57:15 +0800

    add copilot chat keymap

--- lua/custom/plugins/github_copilot.lua
+++ lua/custom/plugins/github_copilot.lua
@@ -10,4 +10,25 @@ return {
       'zbirenbaum/copilot.lua',
     },
   },
+  -- Copilot chat
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    opts = {},
+    dependencies = {
+      { 'zbirenbaum/copilot.lua' }, -- or github/copilot.vim
+      { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
+      {
+        'MeanderingProgrammer/render-markdown.nvim',
+        dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+        opts = {},
+      },
+    },
+    build = 'make tiktoken',
+    config = function()
+      require('CopilotChat').setup {}
+      require('CopilotChat.integrations.cmp').setup()
+      local chat = require 'CopilotChat'
+      vim.keymap.set('n', '<leader>cc', chat.toggle, { desc = 'Toggle [C]opilot [C]hat window.' })
+    end,
+  },
 }
