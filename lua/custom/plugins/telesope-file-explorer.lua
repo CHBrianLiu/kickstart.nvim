e5ca9e44de17fe4bd8fe82347b347d647e259717
Brian Liu <bliu@netskope.com>
2024-12-09 10:11:41 +0800

    update file explorer keymap

--- lua/custom/plugins/telesope-file-explorer.lua
+++ lua/custom/plugins/telesope-file-explorer.lua
@@ -3,6 +3,6 @@ return {
   dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
   config = function()
     require('telescope').load_extension 'file_browser'
-    vim.keymap.set('n', '<space>fb', ':Telescope file_browser<CR>', { desc = '[F]ile [B]rowser' })
+    vim.keymap.set('n', '<space>ft', ':Telescope file_browser<CR>', { desc = '[F]ile [T]tee' })
   end,
 }
