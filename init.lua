d042109e59da5414cda42c92497140821d3593b2
Brian Liu <bliu@netskope.com>
2024-12-11 12:08:41 +0800

    dim the inactive windows

--- init.lua
+++ init.lua
@@ -855,6 +855,7 @@ require('lazy').setup({
     'EdenEast/nightfox.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
+      require('nightfox').setup { options = { dim_inactive = true } }
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
