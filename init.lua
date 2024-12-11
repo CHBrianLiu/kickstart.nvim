194f32d3d487e6c81468a993ad06e1f01703a778
Brian Liu <bliu@netskope.com>
2024-12-11 12:08:18 +0800

    have only one statusline even if there are multiple windows

--- init.lua
+++ init.lua
@@ -71,6 +71,10 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- have only one global status line
+-- i am not used to have status line per window
+vim.opt.laststatus = 3
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
