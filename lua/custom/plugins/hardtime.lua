e6a6ead654cab5b89ef009d1d0ec9c2bd1628b63
Brian Liu <bliu@netskope.com>
2025-04-22 21:34:45 +0800

    delete unused plugins

--- lua/custom/plugins/hardtime.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'm4xshen/hardtime.nvim',
-  dependencies = { 'MunifTanjim/nui.nvim' },
-  opts = {
-    disable_mouse = false,
-    restriction_mode = 'hint',
-    max_count = 15,
-  },
-}
