1e9faf2af01afc0541ae664934f5009d0be6c2ab
brianliu <hong86323@gmail.com>
2025-05-25 23:23:39 +0800

    context: optimize exp

--- lua/custom/plugins/treesitter-context.lua
+++ lua/custom/plugins/treesitter-context.lua
@@ -2,7 +2,12 @@ return {
   'nvim-treesitter/nvim-treesitter-context',
   dependencies = { 'nvim-treesitter/nvim-treesitter' },
   config = function()
-    require('treesitter-context').setup {}
+    require('treesitter-context').setup {
+      -- avoid multiline comments flooding the window
+      multiline_threshold = 1,
+      -- separate the context area and the current editor scope
+      separator = '-',
+    }
     vim.keymap.set('n', '[C', function()
       require('treesitter-context').go_to_context(vim.v.count1)
     end, { silent = true })
