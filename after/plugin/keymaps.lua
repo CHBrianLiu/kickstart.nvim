d44688333f019e61e780c1c20ca6bec02f137705
Brian Liu <bliu@netskope.com>
2025-11-26 16:25:47 +0800

    H/L in operation mode

--- after/plugin/keymaps.lua
+++ after/plugin/keymaps.lua
@@ -1,9 +1,8 @@
 -- BLiu: custom
 -- Local keymaps loaded via 'after/plugin' without modifying init.lua
 -- Use H/L to jump to first/last non-blank character of the current line
-vim.keymap.set({ 'n', 'v' }, 'H', '^', { desc = 'Go to line start' })
-vim.keymap.set({ 'n', 'v' }, 'L', 'g_', { desc = 'Go to line end (non-blank)' })
+vim.keymap.set({ 'n', 'v', 'o' }, 'H', '^', { desc = 'Go to line start' })
+vim.keymap.set({ 'n', 'v', 'o' }, 'L', 'g_', { desc = 'Go to line end (non-blank)' })
 -- BLiu: custom - personal keymaps loaded after core
 -- Map 'jk' in insert mode to escape
 vim.keymap.set('i', 'jk', '<Esc>', { desc = 'Exit insert mode with jk' })
-
