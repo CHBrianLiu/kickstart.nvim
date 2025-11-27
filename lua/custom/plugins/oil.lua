d264c52696df15eff09933cd4598e33502802ba2
Brian Liu <bliu@netskope.com>
2025-11-27 11:19:19 +0800

    Plugin addition: Introduced stevearc/oil.nvim for lightweight floating buffer-based file exploration with a simple key mapping for convenient directory navigation.
    Signed-off-by: Brian Liu <bliu@netskope.com>

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -9,6 +9,29 @@
 return {
   {
     'stevearc/oil.nvim',
-    opts = {}, -- use defaults; customize later if needed
+    -- BLiu: custom - keep config lightweight; we only add a keymap for now.
+    opts = {}, -- defaults are fine; can extend later
+    keys = {
+      {
+        '<leader>f;',
+        function()
+          -- Open Oil rooted at the current buffer's directory (or CWD if unnamed)
+          local ok, oil = pcall(require, 'oil')
+          if not ok then
+            vim.notify('[oil] plugin not loaded', vim.log.levels.WARN)
+            return
+          end
+          local bufname = vim.api.nvim_buf_get_name(0)
+          local path = (bufname ~= '' and vim.fn.fnamemodify(bufname, ':p:h')) or vim.loop.cwd()
+          -- Use Oil API for better UX; fallback to command if API missing
+          if oil.open then
+            oil.open(path)
+          else
+            vim.cmd('Oil ' .. vim.fn.fnameescape(path))
+          end
+        end,
+        desc = 'Oil: open directory of current file',
+      },
+    },
   },
 }
