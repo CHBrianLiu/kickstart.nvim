46390c60bb4967dfb236e90262f340f539897048
Brian Liu <bliu@netskope.com>
2025-11-27 13:46:14 +0800

    keymap to copy file path.

--- after/plugin/keymaps.lua
+++ after/plugin/keymaps.lua
@@ -7,3 +7,15 @@ vim.keymap.set({ 'n', 'v', 'o' }, 'L', 'g_', { desc = 'Go to line end (non-blank
 -- Map 'jk' in insert mode to escape
 vim.keymap.set('i', 'jk', '<Esc>', { desc = 'Exit insert mode with jk' })
 
+-- Copy current file path to the system clipboard
+vim.keymap.set('n', '<leader>fp', function()
+  local path = vim.fn.fnamemodify(vim.fn.expand '%', ':~:.')
+  vim.fn.setreg('+', path)
+  vim.notify('Copied relative path: ' .. path)
+end, { desc = 'Copy relative file path' })
+
+vim.keymap.set('n', '<leader>fP', function()
+  local path = vim.fn.expand '%:p'
+  vim.fn.setreg('+', path)
+  vim.notify('Copied absolute path: ' .. path)
+end, { desc = 'Copy absolute file path' })
