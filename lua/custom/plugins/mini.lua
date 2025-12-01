0ff563b9ed13b7ac4f820eb6d878c768d2ec480c
Brian Liu <bliu@netskope.com>
2025-12-01 16:50:46 +0800

    Enhance mini.lua with 'mini.sessions' configuration.
    Signed-off-by: Brian Liu <bliu@netskope.com>

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -1,4 +1,11 @@
 return { -- Collection of various small independent plugins/modules
   { 'nvim-mini/mini.jump2d', version = '*' },
   { 'nvim-mini/mini.icons', version = '*' },
-}
+  {
+    'nvim-mini/mini.sessions',
+    version = '*',
+    opts = {
+      autoread = true,
+    },
+  },
+} -- Enhanced to include mini.sessions for session management
