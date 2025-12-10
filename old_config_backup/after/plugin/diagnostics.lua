b915568ab5f308a0d755e0ac3e5dc99cda210845
Brian Liu <bliu@netskope.com>
2025-12-10 15:24:03 +0800

    delete the legacy config

--- old_config_backup/after/plugin/diagnostics.lua
+++
@@ -1,5 +0,0 @@
--- Diagnostic icons
-for name, icon in pairs { Error = '󰅙', Info = '󰋼', Hint = '󰌵', Warn = '' } do
-  local hl = 'DiagnosticSign' .. name
-  vim.fn.sign_define(hl, { text = icon, numhl = hl, texthl = hl })
-end
