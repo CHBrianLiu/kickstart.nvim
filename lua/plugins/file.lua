212e8b9fd13f55d2011fc78fedd48b990a29e25b
Brian Liu <bliu@netskope.com>
2025-12-19 10:52:11 +0800

    feat: add Oil floating window keymap for session directory
    - Add <leader>f: keymap to open Oil in floating window at session root
    - Uses vim.fn.getcwd() for clean and predictable behavior
    - Simplifies from project marker detection approach

--- lua/plugins/file.lua
+++ lua/plugins/file.lua
@@ -9,6 +9,11 @@ return {
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
   keys = {
-    { '<leader>f;', '<cmd>Oil<cr>', desc = 'Open Oil' },
+    { '<leader>f;', '<cmd>Oil --float<cr>', desc = 'Open Oil' },
+    {
+      '<leader>f:',
+      function() require('oil').open_float(vim.fn.getcwd()) end,
+      desc = 'Open Oil floating window at session directory',
+    },
   },
 }
