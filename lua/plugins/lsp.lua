40ca89a77bb43823451fc3a464f6fed06c43d72e
Brian Liu <bliu@netskope.com>
2025-12-19 13:28:59 +0800

    do not lazy load blink

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -175,6 +175,7 @@ return {
     'saghen/blink.cmp',
     -- optional: provides snippets for the snippet source
     dependencies = { 'rafamadriz/friendly-snippets' },
+    lazy = false,
 
     -- use a release tag to download pre-built binaries
     version = '1.*',
