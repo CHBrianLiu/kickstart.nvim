3370a08e6b9addaf97a4ff591fb45fc67e2d4995
Brian Liu <bliu@netskope.com>
2026-01-02 11:52:14 +0800

    keymaps(Git): fix some keymaps settings

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -7,11 +7,22 @@ return {
       vim.api.nvim_create_user_command('Gc', 'G commit', {})
       vim.api.nvim_create_user_command('Gcb', 'G checkout -b <args>', { nargs = 1 })
       vim.api.nvim_create_user_command('Glol', 'G log --graph --pretty="%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset"', {})
-      vim.api.nvim_create_user_command('Grb', 'G rebase', {})
+      vim.api.nvim_create_user_command('Grb', 'G rebase <args>', { nargs = 1 })
       vim.api.nvim_create_user_command('Grba', 'G rebase --abort', {})
       vim.api.nvim_create_user_command('Grbc', 'G rebase --continue', {})
-      vim.api.nvim_create_user_command('Grbi', 'G rebase --interactive', {})
+      vim.api.nvim_create_user_command('Grbi', 'G rebase --interactive <args>', { nargs = 1 })
       vim.api.nvim_create_user_command('Gtsc', 'G tag --sort=-creatordate', {})
+
+      vim.api.nvim_create_user_command('Gpsup', function()
+        local branch = vim.fn.FugitiveHead()
+        if branch ~= '' then
+          vim.cmd('G push --set-upstream origin ' .. branch)
+        else
+          vim.notify('Not in a git repository or no branch checked out', vim.log.levels.ERROR)
+        end
+      end, { desc = 'Push and set upstream to current branch' })
+
+      vim.api.nvim_create_user_command('GpF', 'G push --force', { desc = 'Force push' })
     end,
     keys = {
       {
@@ -26,25 +37,8 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     opts = {},
     keys = {
-      {
-        '<leader>gl',
-        function()
-          require('gitlinker').get_buf_range_url('n', {
-            action = require('gitlinker.actions').copy_to_clipboard,
-          })
-        end,
-        desc = 'Copy remote [G]it [L]ink (current line/range)',
-      },
-      {
-        '<leader>gl',
-        function()
-          require('gitlinker').get_buf_range_url('v', {
-            action = require('gitlinker.actions').copy_to_clipboard,
-          })
-        end,
-        mode = 'v',
-        desc = 'Copy remote [G]it [L]ink for selection',
-      },
+      { '<leader>gl', '<cmd>GitLink<cr>', mode = { 'n', 'v' }, desc = 'Yank git link' },
+      { '<leader>go', '<cmd>GitLink!<cr>', mode = { 'n', 'v' }, desc = 'Open git link' },
     },
   },
 
@@ -53,6 +47,47 @@ return {
     dependencies = {
       'nvim-lua/plenary.nvim',
       'tpope/vim-fugitive',
+    },
+  },
+  {
+    'pwntester/octo.nvim',
+    cmd = 'Octo',
+    opts = {
+      picker = 'telescope',
+      -- bare Octo command opens picker of commands
+      enable_builtin = true,
+    },
+    keys = {
+      -- {
+      --   '<leader>oi',
+      --   '<CMD>Octo issue list<CR>',
+      --   desc = 'List GitHub Issues',
+      -- },
+      -- {
+      --   '<leader>op',
+      --   '<CMD>Octo pr list<CR>',
+      --   desc = 'List GitHub PullRequests',
+      -- },
+      -- {
+      --   '<leader>od',
+      --   '<CMD>Octo discussion list<CR>',
+      --   desc = 'List GitHub Discussions',
+      -- },
+      -- {
+      --   '<leader>on',
+      --   '<CMD>Octo notification list<CR>',
+      --   desc = 'List GitHub Notifications',
+      -- },
+      -- {
+      --   '<leader>os',
+      --   function() require('octo.utils').create_base_search_command { include_current_repo = true } end,
+      --   desc = 'Search GitHub',
+      -- },
+    },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+      'nvim-tree/nvim-web-devicons',
     },
   },
 }
