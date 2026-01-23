3053e6760106a44e264f25efa7c48e7e2cb7b028
Brian Liu <bliu@netskope.com>
2026-01-23 14:12:08 +0800

    feat(ui): enable lsp doc border in noice
    Also normalizes quotes for todo-comments dependency.

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -217,6 +217,7 @@ return {
         command_palette = true, -- position the cmdline and popupmenu together
         long_message_to_split = true, -- long messages will be sent to a split
         inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = true,
       },
     },
     dependencies = {
@@ -229,8 +230,8 @@ return {
     },
   },
   {
-    "folke/todo-comments.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    'folke/todo-comments.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
     opts = {},
   },
 }
